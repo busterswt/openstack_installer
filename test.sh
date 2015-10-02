c34212df7151d58ddfb7b2d80139e8f7f6b7c0c4
James Denton <james.denton@rackspace.com>
2015-10-01 22:24:57 -0500

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# load the in.ini INI file to current BASH - quoted to preserve line breaks
-eval "$(cat config.ini  | ./scripts/ini2arr.py)"
-
-# test it:
-echo ${compute02[mgmt_addr]}
